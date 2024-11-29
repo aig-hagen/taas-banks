b59f22db70e8cfa36aa59dbc41f120753847e420
Matthias Thimm <matthias.thimm@fernuni-hagen.de>
2024-11-29 13:21:26 +0100

    added baseline approach

--- taas-banks.cpp
+++ taas-banks.cpp
@@ -94,8 +94,114 @@ void solve_baseline_no_comp(struct TaskSpecification *task, struct AAF* aaf) {
 
 /* ============================================================================================================== */
 /* ============================================================================================================== */
+std::set<std::set<int>> all_exts(struct AAF* aaf){
+    // Base encoding for stable semantics
+    IpasirSolver solver;
+    sat__init(solver, aaf->number_of_arguments);
+    // Rules for stable semantics
+    for (int a = 1; a <= aaf->number_of_arguments; a++) {
+        if (aaf->number_of_attackers[a-1] == 0) {
+            sat__addClause1(solver,a);
+        } else {
+            std::vector<int> attackersOr;
+            for(GSList* node2 = aaf->parents[a-1]; node2 != NULL; node2 = node2->next){
+                attackersOr.push_back(*(int*)node2->data);
+                sat__addClause2(solver,-a,-*(int*)node2->data);
+            }
+            attackersOr.push_back(a);
+            attackersOr.push_back(0);
+            sat__addClauseZTVec(solver,attackersOr);
+        }
+    }
+    // Iteratively find and add extensions
+    std::set<std::set<int>> exts;
+    while (true) {
+        int sat = sat__solve(solver);
+        if (sat == 20) {
+            break; // No more extensions
+        }
+        std::set<int> ext;
+        std::vector<int> clause;
+        for (int a = 1; a <= aaf->number_of_arguments; a++){
+            if (sat__get(solver,a) > 0) {
+                ext.insert(a);
+                clause.push_back(-a);
+            }
+        }
+        clause.push_back(0);
+        exts.insert(ext);
+        sat__addClauseZTVec(solver,clause);
+    }
+    return exts;
+}
+
 void solve_baseline_ext_expl(struct TaskSpecification *task, struct AAF* aaf) {
-    printf("BASELINE-EXT-EXPLICIT\n");
+    if(strcmp(task->problem,"BATCH-ST") == 0){
+        std::vector<std::string> tasks;
+        std::vector<int> args;
+        for(int j = 0; j < task->number_of_additional_arguments; j++){
+            tasks.push_back(task->additional_keys[j]);
+            args.push_back(atoi(task->additional_values[j]));
+        }
+        // determine all extensions
+        std::set<std::set<int>> exts = all_exts(aaf);
+        for(int i = 0; i < tasks.size(); i++){
+            if(tasks[i].compare("DS") == 0){
+                bool is_accepted = true;
+                for(std::set<int> ext: exts){
+                    if(ext.find(args[i]) == ext.end()){
+                        is_accepted = false;
+                        break;
+                    }
+                }
+                std::cout << (is_accepted ? "YES" : "NO" ) << std::endl;
+            }else if(tasks[i].compare("DC") == 0){
+                bool is_accepted = false;
+                for(std::set<int> ext: exts){
+                    if(ext.find(args[i]) != ext.end()){
+                        is_accepted = true;
+                        break;
+                    }
+                }
+                std::cout << ( is_accepted ? "YES" : "NO" ) << std::endl;
+            }else {
+                std::cout << "UNKNOWN QUERY" << std::endl;
+            }
+        }
+      return;
+    }else if(strcmp(task->problem,"DS-ST") == 0){
+        std::set<std::set<int>> exts = all_exts(aaf);
+        int arg = 0;
+        for(int j = 0; j < task->number_of_additional_arguments; j++){
+            if(strcmp(task->additional_keys[j],"-a") == 0)
+                arg = atoi(task->additional_values[j]);
+        }
+        bool is_accepted = true;
+        for(std::set<int> ext: exts){
+            if(ext.find(arg) == ext.end()){
+                is_accepted = false;
+                break;
+            }
+        }
+        std::cout << ( is_accepted ? "YES" : "NO" ) << std::endl;
+        return;
+    }else if(strcmp(task->problem,"DC-ST") == 0){
+        std::set<std::set<int>> exts = all_exts(aaf);
+        int arg = 0;
+        for(int j = 0; j < task->number_of_additional_arguments; j++){
+            if(strcmp(task->additional_keys[j],"-a") == 0)
+                arg = atoi(task->additional_values[j]);
+        }
+        bool is_accepted = false;
+        for(std::set<int> ext: exts){
+            if(ext.find(arg) != ext.end()){
+                is_accepted = true;
+                break;
+            }
+        }
+        std::cout << ( is_accepted ? "YES" : "NO" ) << std::endl;
+        return;
+    }
 }
 
 /* ============================================================================================================== */
